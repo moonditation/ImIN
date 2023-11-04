eefcf141702e3de7c37b211f1bdf4d19ffcc4ebd
woohyuk <7dngur7@naver.com>
2023-11-04 22:19:19 +0900

    noti page and find id pw

---
+++ ssainfe_project/app/src/main/java/com/example/myapplication/find_id_password.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class find_id_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_password);
+    }
+}
---
+++ ssainfe_project/app/src/main/java/com/example/myapplication/make_promise.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class make_promise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_promise);
+    }
+}
